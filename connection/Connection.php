0ecb69f3ebad8339c273226b98c77b3a11b76196
Matias Alicata <matiasalicata@gmail.com>
2023-08-25 21:33:29 -0300

    add new connection

---
+++ connection/Connection.php
@@ -0,0 +1,11 @@
+<?php
+
+    class Connection extends Mysqli {
+        function __construct(){
+            parent::__construct('localhost', 'root', '123456', 'api_rest');
+            $this->set_charset('utf8');
+            $this->connect_error == NULL ? 'Conexión exítosa a la DB' : die('Error al conectarse a la DB');
+        }//end __construct
+    }// end class Connection
+
+?>
